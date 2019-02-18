6e509d9f2531f9c8688d751ff79e25be39e403a7
pooja kumawat <poojakumu7@gmail.com>
2019-02-18 11:43:53 +0530

    'added'

---
+++ src/main/java/HelloWorldMaven/MavenJob/App.java
@@ -0,0 +1,13 @@
+package HelloWorldMaven.MavenJob;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
